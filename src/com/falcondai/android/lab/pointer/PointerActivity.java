f6d60b78f8bc33ce86d4248890e91b700c9b87a4
Falcon Dai <me@falcondai.com>
2012-05-10 00:05:36 -0500

    byte msg. less hiccups.

--- src/com/falcondai/android/lab/pointer/PointerActivity.java
+++ src/com/falcondai/android/lab/pointer/PointerActivity.java
@@ -88,24 +88,22 @@ public class PointerActivity extends Activity {
 		// TODO rewrite the sensor acquisition with NDK
 		rv_sel = new SensorEventListener() {
 			private long t = 0;
-			private StringBuilder sb = new StringBuilder();
+//			private StringBuilder sb = new StringBuilder();
 			private long mt = 0;
-
-			// String s = "";
-			// String info_text = "";
-
+			private byte[] buf = new byte[8+3*4];
+			
 			@Override
 			public void onSensorChanged(SensorEvent se) {
-				sb.setLength(0);
-				sb.append("timestamp: ").append(se.timestamp).append('\n');
-				sb.append(se.values[0]).append('\n');
-				sb.append(se.values[1]).append('\n');
-				sb.append(se.values[2]).append('\n');
-				sb.append("magnitude: ").append(magnitude(se.values))
-						.append('\n');
-				sb.append("update rate: ").append(1e9 / (se.timestamp - t))
-						.append("Hz\n");
-				sb.append("maximnum wait: ").append(mt / 1e6).append("ms\n");
+//				sb.setLength(0);
+//				sb.append("timestamp: ").append(se.timestamp).append('\n');
+//				sb.append(se.values[0]).append('\n');
+//				sb.append(se.values[1]).append('\n');
+//				sb.append(se.values[2]).append('\n');
+//				sb.append("magnitude: ").append(magnitude(se.values))
+//						.append('\n');
+//				sb.append("update rate: ").append(1e9 / (se.timestamp - t))
+//						.append("Hz\n");
+//				sb.append("maximnum wait: ").append(mt / 1e6).append("ms\n");
 				if (t == 0) {
 					t = se.timestamp;
 				}
@@ -119,13 +117,13 @@ public class PointerActivity extends Activity {
 
 				t = se.timestamp;
 
-				info.setText(sb.toString());
+//				info.setText(sb.toString());
 				// info.setText(info_text);
 				// Log.d(tag, sb.toString());
 
 				try {
 					// package the sensor event
-					dp = packageSensorEvent(se);
+					dp = packageSensorEvent(se, buf);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -143,22 +141,22 @@ public class PointerActivity extends Activity {
 	public void onResume() {
 		super.onResume();
 
-		int x = Float.floatToIntBits(100.0f);
-		int y = 200;
-		long t = 2L;
-//		byte[] buf = { (byte) ((x >>> 0) & 0xff), (byte) ((x >>> 8) & 0xff),
-//				(byte) ((x >>> 16) & 0xff), (byte) ((x >>> 24) & 0xff),
-//				(byte) ((y >>> 0) & 0xff), (byte) ((y >>> 8) & 0xff),
-//				(byte) ((y >>> 16) & 0xff), (byte) ((y >>> 24) & 0xff) };
-		byte[] buf = { (byte) ((t >>> 0) & 0xff),
-				(byte) ((t >>> 8) & 0xff),
-				(byte) ((t >>> 16) & 0xff),
-				(byte) ((t >>> 24) & 0xff),
-				(byte) ((t >>> 32) & 0xff),
-				(byte) ((t >>> 40) & 0xff),
-				(byte) ((t >>> 48) & 0xff),
-				(byte) ((t >>> 56) & 0xff), };
-//		dp = new DatagramPacket(buf, 8);
+//		int x = Float.floatToRawIntBits(100.0f);
+//		int y = 200;
+//		long t = 1000L;
+//		 byte[] buf = { (byte) ((x >>> 0) & 0xff), (byte) ((x >>> 8) & 0xff),
+//		 (byte) ((x >>> 16) & 0xff), (byte) ((x >>> 24) & 0xff),
+//		 (byte) ((y >>> 0) & 0xff), (byte) ((y >>> 8) & 0xff),
+//		 (byte) ((y >>> 16) & 0xff), (byte) ((y >>> 24) & 0xff) };
+//		byte[] buf = { (byte) ((t >>> 0) & 0xff), (byte) ((t >>> 8) & 0xff),
+//				(byte) ((t >>> 16) & 0xff), (byte) ((t >>> 24) & 0xff),
+//				(byte) ((t >>> 32) & 0xff), (byte) ((t >>> 40) & 0xff),
+//				(byte) ((t >>> 48) & 0xff), (byte) ((t >>> 56) & 0xff), };
+//		byte[] buf = new byte[16];
+//		writeByteBuffer(buf, 0, 123456789L);
+//		writeByteBuffer(buf, 8, 3.1415926f);
+//		writeByteBuffer(buf, 12, 2.17f);
+//		dp = new DatagramPacket(buf, buf.length);
 
 		sm.registerListener(rv_sel, rv, SensorManager.SENSOR_DELAY_FASTEST);
 	}
@@ -186,30 +184,45 @@ public class PointerActivity extends Activity {
 		return (float) Math.sqrt(s);
 	}
 
-	protected DatagramPacket packageSensorEvent(SensorEvent se)
+	protected DatagramPacket packageSensorEvent(SensorEvent se, byte[] buf)
 			throws UnknownHostException {
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append(se.timestamp).append(' ').append(se.values[0]).append(' ').append(se.values[1]).append(' ').append(se.values[2]);
-		byte[] buffer = sb.toString().getBytes(); 
-		// TODO package the data more compactly
-		int f0 = Float.floatToIntBits(se.values[0]);
-		int f1 = Float.floatToIntBits(se.values[1]);
-		int f2 = Float.floatToIntBits(se.values[2]);
-		
-//		byte[] buffer = { (byte) ((se.timestamp >>> 0) & 0xff),
-//				(byte) ((se.timestamp >>> 8) & 0xff),
-//				(byte) ((se.timestamp >>> 16) & 0xff),
-//				(byte) ((se.timestamp >>> 24) & 0xff),
-//				(byte) ((se.timestamp >>> 32) & 0xff),
-//				(byte) ((se.timestamp >>> 40) & 0xff),
-//				(byte) ((se.timestamp >>> 48) & 0xff),
-//				(byte) ((se.timestamp >>> 56) & 0xff), 
-//				(byte) ((f0 >>> 0) & 0xff), (byte) ((f0 >>> 8) & 0xff), (byte) ((f0 >>> 16) & 0xff), (byte) ((f0 >>> 24) & 0xff),
-//				(byte) ((f1 >>> 0) & 0xff),(byte) ((f1 >>> 8) & 0xff),(byte) ((f1 >>> 16) & 0xff),(byte) ((f1 >>> 24) & 0xff),
-//				(byte) ((f2 >>> 0) & 0xff),(byte) ((f2 >>> 8) & 0xff),(byte) ((f2 >>> 16) & 0xff),(byte) ((f2 >>> 24) & 0xff),
-//				};
 
-		return new DatagramPacket(buffer, buffer.length);
+//		StringBuilder sb = new StringBuilder();
+//		sb.append(se.timestamp).append(' ').append(se.values[0]).append(' ')
+//				.append(se.values[1]).append(' ').append(se.values[2]);
+//		byte[] buffer = sb.toString().getBytes();
+		
+		// TODO package the data more compactly
+		writeByteBuffer(buf, 0, se.timestamp);
+		writeByteBuffer(buf, 8, se.values[0]);
+		writeByteBuffer(buf, 12, se.values[1]);
+		writeByteBuffer(buf, 16, se.values[2]);
+
+		return new DatagramPacket(buf, buf.length);
+	}
+
+	protected void writeByteBuffer(byte[] buf, int offset, long n) {
+		if (offset + 8 > buf.length) {
+			// the buffer is not big enough for the data
+			// TODO throws an exception
+			return;
+		}
+
+		for (int i = 0; i < 8; i++) {
+			buf[offset + i] = (byte) ((n >>> i * 8) & 0xff);
+		}
+	}
+	
+	protected void writeByteBuffer(byte[] buf, int offset, float f) {
+		if (offset + 4 > buf.length) {
+			// the buffer is not big enough for the data
+			// TODO throws an exception
+			return;
+		}
+
+		int n = Float.floatToRawIntBits(f);
+		for (int i = 0; i < 4; i++) {
+			buf[offset + i] = (byte) ((n >>> i * 8) & 0xff);
+		}
 	}
 }
